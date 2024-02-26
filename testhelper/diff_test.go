35056cd91ac25b6ce81babb50aadcbb9b73c5d39
schen07 <shangkuei.chen@yahooinc.com>
2024-02-26 16:50:09 +0800

    version 0.0.1

--- testhelper/diff_test.go
+++ testhelper/diff_test.go
@@ -20,10 +20,9 @@ func TestEqual(t *testing.T) {
 		opts []cmp.Option
 	}
 	tests := []struct {
-		name     string
-		args     args
-		wantDiff string
-		wantOk   bool
+		name   string
+		args   args
+		wantOk bool
 	}{
 		{
 			name: "same struct but different unexported field",
@@ -38,7 +37,6 @@ func TestEqual(t *testing.T) {
 				},
 				opts: []cmp.Option{cmp.AllowUnexported(equalA{})},
 			},
-			wantDiff: "Diff(-got,+want):\n\t  helper.equalA{\n\t- \tunexported: \"unexported\",\n\t+ \tunexported: \"\",\n\t  \tExported:   \"exported\",\n\t  }",
 		},
 		{
 			name: "same struct",
@@ -61,17 +59,12 @@ func TestEqual(t *testing.T) {
 				got:  equalA{},
 				want: equalB{},
 			},
-			wantDiff: "Diff(-got,+want):\n\t  any(\n\t- \thelper.equalA{},\n\t+ \thelper.equalB{},\n\t  )",
-			wantOk:   false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotDiff, gotOk := Equal(tt.args.got, tt.args.want, tt.args.opts...)
-			if got, want := gotDiff, tt.wantDiff; got != want {
-				t.Errorf("Equal() got = %v, want %v", got, want)
-			}
-			t.Log(cmp.Diff(gotDiff, tt.wantDiff))
+			t.Log(gotDiff)
 			if got, want := gotOk, tt.wantOk; got != want {
 				t.Errorf("Equal() got = %v, want %v", got, want)
 			}
